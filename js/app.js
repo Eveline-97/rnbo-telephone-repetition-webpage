163b9d4eee2764d63ad2d3ba65f7e64a7849e2c9
Eveline Vervliet <57260992+Eveline-97@users.noreply.github.com>
2024-08-19 12:59:55 +0200

    first commit with adaptations from starting rnbo project, basic app working

---
+++ js/app.js
@@ -0,0 +1,350 @@
+async function setup() {
+    if (!isMobileOrTablet()) {
+        alert("This app only works on a mobile touch device.");
+    }
+    const patchExportURL = "export/repeating-telephone.export.json";
+
+    // Create AudioContext
+    const WAContext = window.AudioContext || window.webkitAudioContext;
+    const context = new WAContext();
+
+    // Create gain node and connect it to audio output
+    const outputNode = context.createGain();
+    outputNode.connect(context.destination);
+
+    // Fetch the exported patcher
+    let response, patcher;
+    try {
+        response = await fetch(patchExportURL);
+        patcher = await response.json();
+
+        if (!window.RNBO) {
+            // Load RNBO script dynamically
+            // Note that you can skip this by knowing the RNBO version of your patch
+            // beforehand and just include it using a <script> tag
+            await loadRNBOScript(patcher.desc.meta.rnboversion);
+        }
+
+    } catch (err) {
+        const errorContext = {
+            error: err
+        };
+        if (response && (response.status >= 300 || response.status < 200)) {
+            errorContext.header = `Couldn't load patcher export bundle`,
+                errorContext.description = `Check app.js to see what file it's trying to load. Currently it's` +
+                ` trying to load "${patchExportURL}". If that doesn't` +
+                ` match the name of the file you exported from RNBO, modify` +
+                ` patchExportURL in app.js.`;
+        }
+        if (typeof guardrails === "function") {
+            guardrails(errorContext);
+        } else {
+            throw err;
+        }
+        return;
+    }
+
+    // (Optional) Fetch the dependencies
+    let dependencies = [];
+    try {
+        const dependenciesResponse = await fetch("export/dependencies.json");
+        dependencies = await dependenciesResponse.json();
+
+        // Prepend "export" to any file dependenciies
+        dependencies = dependencies.map(d => d.file ? Object.assign({}, d, { file: "export/" + d.file }) : d);
+    } catch (e) { }
+
+    // Create the device
+    let device;
+    try {
+        device = await RNBO.createDevice({ context, patcher });
+    } catch (err) {
+        if (typeof guardrails === "function") {
+            guardrails({ error: err });
+        } else {
+            throw err;
+        }
+        return;
+    }
+
+    // (Optional) Load the samples
+    if (dependencies.length)
+        await device.loadDataBufferDependencies(dependencies);
+
+    // Connect the device to the web audio graph
+    device.node.connect(outputNode);
+
+    // (Optional) Automatically create sliders for the device parameters
+    makeSliders(device);
+
+    // (Optional) Create a form to send messages to RNBO inputs
+    makeInportForm(device);
+
+    // (Optional) Attach listeners to outports so you can log messages from the RNBO patcher
+    //attachOutports(device);
+
+    document.body.onclick = () => {
+        context.resume();
+    }
+
+    // Skip if you're not using guardrails.js
+    if (typeof guardrails === "function")
+        guardrails();
+}
+
+function loadRNBOScript(version) {
+    return new Promise((resolve, reject) => {
+        if (/^\d+\.\d+\.\d+-dev$/.test(version)) {
+            throw new Error("Patcher exported with a Debug Version!\nPlease specify the correct RNBO version to use in the code.");
+        }
+        const el = document.createElement("script");
+        el.src = "https://c74-public.nyc3.digitaloceanspaces.com/rnbo/" + encodeURIComponent(version) + "/rnbo.min.js";
+        el.onload = resolve;
+        el.onerror = function (err) {
+            console.log(err);
+            reject(new Error("Failed to load rnbo.js v" + version));
+        };
+        document.body.append(el);
+    });
+}
+
+function makeSliders(device) {
+    let pdiv = document.getElementById("rnbo-parameter-sliders");
+    let noParamLabel = document.getElementById("no-param-label");
+    if (noParamLabel && device.numParameters > 0) pdiv.removeChild(noParamLabel);
+
+    // This will allow us to ignore parameter update events while dragging the slider.
+    let isDraggingSlider = false;
+    let uiElements = {};
+
+    device.parameters.forEach(param => {
+        // Subpatchers also have params. If we want to expose top-level
+        // params only, the best way to determine if a parameter is top level
+        // or not is to exclude parameters with a '/' in them.
+        // You can uncomment the following line if you don't want to include subpatcher params
+
+        //if (param.id.includes("/")) return;
+
+        // Create a label, an input slider and a value display
+        let label = document.createElement("label");
+        let slider = document.createElement("input");
+        let text = document.createElement("input");
+        let sliderContainer = document.createElement("div");
+        sliderContainer.appendChild(label);
+        sliderContainer.appendChild(slider);
+        sliderContainer.appendChild(text);
+
+        // Add a name for the label
+        label.setAttribute("name", param.name);
+        label.setAttribute("for", param.name);
+        label.setAttribute("class", "param-label");
+        label.textContent = `${param.name}: `;
+
+        // Make each slider reflect its parameter
+        slider.setAttribute("type", "range");
+        slider.setAttribute("class", "param-slider");
+        slider.setAttribute("id", param.id);
+        slider.setAttribute("name", param.name);
+        slider.setAttribute("min", param.min);
+        slider.setAttribute("max", param.max);
+        if (param.steps > 1) {
+            slider.setAttribute("step", (param.max - param.min) / (param.steps - 1));
+        } else {
+            slider.setAttribute("step", (param.max - param.min) / 1000.0);
+        }
+        slider.setAttribute("value", param.value);
+
+        // Make a settable text input display for the value
+        text.setAttribute("value", param.value.toFixed(1));
+        text.setAttribute("type", "text");
+
+        // Make each slider control its parameter
+        slider.addEventListener("pointerdown", () => {
+            isDraggingSlider = true;
+        });
+        slider.addEventListener("pointerup", () => {
+            isDraggingSlider = false;
+            slider.value = param.value;
+            text.value = param.value.toFixed(1);
+        });
+        slider.addEventListener("input", () => {
+            let value = Number.parseFloat(slider.value);
+            param.value = value;
+        });
+        slider.addEventListener('change', () => {
+            let value = Number.parseFloat(slider.value);
+            text.value = param.value.toFixed(1);
+            param.value = value;
+        })
+
+        // Make the text box input control the parameter value as well
+        text.addEventListener("keydown", (ev) => {
+            if (ev.key === "Enter") {
+                let newValue = Number.parseFloat(text.value);
+                if (isNaN(newValue)) {
+                    text.value = param.value;
+                } else {
+                    newValue = Math.min(newValue, param.max);
+                    newValue = Math.max(newValue, param.min);
+                    text.value = newValue;
+                    param.value = newValue;
+                }
+            }
+        });
+
+        // Store the slider and text by name so we can access them later
+        uiElements[param.id] = { slider, text };
+
+        // Add the slider element
+        pdiv.appendChild(sliderContainer);
+    });
+
+    // Listen to parameter changes from the device
+    // Dit zou eigenlijk al genoeg moeten zijn om 
+    device.parameterChangeEvent.subscribe(param => {
+        if (!isDraggingSlider)
+            uiElements[param.id].slider.value = param.value;
+        uiElements[param.id].text.value = param.value.toFixed(1);
+    });
+}
+
+function makeInportForm(device) {
+    const idiv = document.getElementById("rnbo-inports");
+    const inportSelect = document.getElementById("inport-select");
+    const inportText = document.getElementById("inport-text");
+    const inportForm = document.getElementById("inport-form");
+    let inportTag = null;
+
+    // Device messages correspond to inlets/outlets or inports/outports
+    // You can filter for one or the other using the "type" of the message
+    const messages = device.messages;
+    const inports = messages.filter(message => message.type === RNBO.MessagePortType.Inport);
+
+    if (inports.length === 0) {
+        return;
+    } else {
+        //dit verplaatst want wil geen tekst
+        //idiv.removeChild(document.getElementById("inport-form"));
+        inports.forEach(inport => {
+            const option = document.createElement("option");
+            option.innerText = inport.tag;
+            inportSelect.appendChild(option);
+        });
+        inportSelect.onchange = () => inportTag = inportSelect.value;
+        inportTag = inportSelect.value;
+
+
+        inportForm.onsubmit = (ev) => {
+            // Do this or else the page will reload
+            ev.preventDefault();
+
+            // Turn the text into a list of numbers (RNBO messages must be numbers, not text)
+            const values = inportText.value.split(/\s+/).map(s => parseFloat(s));
+
+            // Send the message event to the RNBO device
+            let messageEvent = new RNBO.MessageEvent(RNBO.TimeNow, inportTag, values);
+            device.scheduleEvent(messageEvent);
+        }
+
+        /*
+        PARAMETERS
+        0 speed
+        1 start
+        2 length
+        3 index
+        */
+        //let speed = device.parameters[0];
+        let retrigger = device.parameters[1]
+        //let start = device.parameters[2];
+        //let length = device.parameters[3];
+        let index = device.parameters[4];
+
+        index.value = 2;
+
+        let alreadyFullscreen = false;
+        let blob = document.getElementById('blob');
+
+        document.addEventListener('touchstart', e => {
+            //first time
+            //fullscreen
+            if (document.getElementById('rnbo-root').requestFullscreen) {
+                if (!alreadyFullscreen) {
+                    document.getElementById('rnbo-root').requestFullscreen();
+                }
+            } else {
+                console.log('No fullscreen method found');
+            }
+            // Turn the text into a list of numbers (RNBO messages must be numbers, not text)
+            const values = inportText.value.split(/\s+/).map(s => parseFloat(s));
+
+            document.getElementById('rnbo-root').style.backgroundColor = 'white';
+            let touch = e.targetTouches[0];
+            let x = touch.clientX;
+            let y = touch.clientY;
+
+            blob.classList.remove('hidden');
+            blob.style.left = x-10 + 'px';
+            blob.style.top = y-10 + 'px';
+
+            retrigger.value = scale(0, screen.height, 10, 330, y);
+            index.value = scale(0, screen.width, 0, 8, x);
+
+            // Send the message event to the RNBO device
+            let messageEvent = new RNBO.MessageEvent(RNBO.TimeNow, inportTag, 1);
+            device.scheduleEvent(messageEvent);
+        })
+
+        document.addEventListener('touchmove', e => {
+            //idee is dat bij touchmove het event al is gestart (bij touchstart) en hier enkel nog de speed verandert kan worden
+            let x = e.targetTouches[0].clientX;
+            let y = e.targetTouches[0].clientY;
+            document.getElementById('rnbo-root').style.backgroundColor = 'red';
+            retrigger.value = scale(0, screen.height, 10, 330, y);
+            index.value = scale(0, screen.width, 0, 8, x);
+
+
+            blob.style.left = x-10 + 'px';
+            blob.style.top = y-10 + 'px';
+        })
+        document.addEventListener('touchend', () => {
+            document.getElementById('rnbo-root').style.backgroundColor = 'black';
+            let messageEvent = new RNBO.MessageEvent(RNBO.TimeNow, inportTag, 0);
+            device.scheduleEvent(messageEvent);
+            blob.classList.add('hidden');
+        })
+    }
+    console.log(device.parameters[1].value);
+}
+
+
+function attachOutports(device) {
+    const outports = device.outports;
+    if (outports.length < 1) {
+        document.getElementById("rnbo-console").removeChild(document.getElementById("rnbo-console-div"));
+        return;
+    }
+
+    document.getElementById("rnbo-console").removeChild(document.getElementById("no-outports-label"));
+    device.messageEvent.subscribe((ev) => {
+
+        // Ignore message events that don't belong to an outport
+        if (outports.findIndex(elt => elt.tag === ev.tag) < 0) return;
+
+        // Message events have a tag as well as a payload
+        console.log(`${ev.tag}: ${ev.payload}`);
+
+        document.getElementById("rnbo-console-readout").innerText = `${ev.tag}: ${ev.payload}`;
+    });
+}
+
+function scale(minIn, maxIn, minOut, maxOut, value) {
+    return value / (maxIn - minIn) * (maxOut - minOut);
+}
+
+function isMobileOrTablet() {
+    let check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+}
+
+setup();
